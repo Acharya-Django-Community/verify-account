af9bc0c0ebb3a5743899b44e82148615e7654bff
axrorback <axrorback@gmail.com>
2026-03-26 13:18:57 +0500

    Add password reset functionality to login page and URLs

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import register , verify_otp
-from django.contrib.auth.views import LoginView , LogoutView
+from django.contrib.auth.views import LoginView , LogoutView , PasswordResetView , PasswordResetDoneView , PasswordResetConfirmView , PasswordResetCompleteView
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('verify-otp/', verify_otp, name='verify_otp'),
+    path('password-reset/',PasswordResetView.as_view(template_name='reset.html'),name='password_reset'),
+    path('reset-done/',PasswordResetDoneView.as_view(template_name='reset.done.html'),name='password_reset_done'),
+    path('confirm-reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='confirm.html'),name='password_reset_confirm'),
+    path('complete-reset-password/',PasswordResetCompleteView.as_view(template_name='reset_complete.html'),name='password_reset_complete')
+
 ]
